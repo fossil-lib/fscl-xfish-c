c3cb757637cdeb491dd6167c7eef03d569bda064
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-02-18 13:33:39 -0600

    Update nlg.c

--- code/source/nlg.c
+++ code/source/nlg.c
@@ -43,20 +43,36 @@ static char *generate_source_code_documentation(jellyfish_model *language_model,
     // Sample implementation: Adding comments with line numbers to the input code
     const char *comment_template = "// Line %d: %s\n";
     const char *python_comment_template = "# Line %d: %s\n";
+    const char *multiline_comment_start = "/*";
+    const char *multiline_comment_end = "*/";
     const char *line_delimiter = "\n";
-    
+
     // Split input code into lines
     char *code_copy = custom_nlg_strdup(input); // Creating a copy since strtok modifies the string
     char *line = strtok(code_copy, line_delimiter);
     char *output = malloc(strlen(input) * 3); // Allocate some initial space for output
-    
+
     int line_number = 1;
-    
+    int in_multiline_comment = 0;
+
     // Iterate through each line and generate comments with line numbers
     while (line != NULL) {
+        // Check if the line is inside a multiline comment
+        if (strstr(line, multiline_comment_start) != NULL) {
+            in_multiline_comment = 1;
+        }
+
         // Generate a comment for the line
         char *comment;
-        if (line[0] == '#') {
+        if (in_multiline_comment) {
+            comment = malloc(strlen(line) + strlen(comment_template) + 5); // Extra space for line number
+            sprintf(comment, comment_template, line_number, line);
+            
+            // Check if the multiline comment ends on this line
+            if (strstr(line, multiline_comment_end) != NULL) {
+                in_multiline_comment = 0;
+            }
+        } else if (line[0] == '#') {
             comment = malloc(strlen(line) + strlen(python_comment_template) + 5); // Extra space for line number
             sprintf(comment, python_comment_template, line_number, line + 1); // Skip '#' character
         } else {
@@ -67,44 +83,12 @@ static char *generate_source_code_documentation(jellyfish_model *language_model,
         // Append the comment to the output
         strcat(output, comment);
         free(comment);
-        
+
         // Move to the next line
         line = strtok(NULL, line_delimiter);
         line_number++;
     }
-    
-    free(code_copy);
-    return output;
-}
 
-// Function to generate a description
-static char *generate_description(jellyfish_model *language_model, const char *input) {
-    // Sample implementation: Generating detailed descriptions for each symbol in the input
-    const char *description_template = "Description for '%s': %s\n";
-    
-    char *output = malloc(strlen(input) * 2); // Allocate some initial space for output
-    
-    // Split input code into symbols (words)
-    char *code_copy = custom_nlg_strdup(input); // Creating a copy since strtok modifies the string
-    char *symbol = strtok(code_copy, " \t\n\r"); // Split by space, tab, newline, and carriage return
-    
-    // Iterate through each symbol and generate descriptions
-    while (symbol != NULL) {
-        // Generate a description for the symbol
-        char *description = fscl_jellyfish_generate(language_model, Description, symbol);
-        char *formatted_description = malloc(strlen(symbol) + strlen(description) + strlen(description_template));
-        sprintf(formatted_description, description_template, symbol, description);
-        
-        // Append the formatted description to the output
-        strcat(output, formatted_description);
-        
-        free(description);
-        free(formatted_description);
-        
-        // Move to the next symbol
-        symbol = strtok(NULL, " \t\n\r");
-    }
-    
     free(code_copy);
     return output;
 }
@@ -112,28 +96,28 @@ static char *generate_description(jellyfish_model *language_model, const char *i
 // Function to generate pseudocode
 static char *generate_pseudocode(jellyfish_model *language_model, const char *input) {
     // Sample implementation: Convert input code to pseudocode by simplifying syntax
-    // For demonstration purposes, this implementation simply replaces 'if' with 'when'
+    // For demonstration purposes, this implementation replaces 'if' with 'when'
     // and 'for' with 'repeat' in the input code.
     char *pseudocode = custom_nlg_strdup(input); // Creating a copy since we modify the string
     char *if_keyword = "if";
     char *when_keyword = "when";
     char *for_keyword = "for";
     char *repeat_keyword = "repeat";
-    
+
     // Replace 'if' with 'when'
     char *if_occurrence = pseudocode;
     while ((if_occurrence = strstr(if_occurrence, if_keyword)) != NULL) {
         strncpy(if_occurrence, when_keyword, strlen(when_keyword));
         if_occurrence += strlen(when_keyword);
     }
-    
+
     // Replace 'for' with 'repeat'
     char *for_occurrence = pseudocode;
     while ((for_occurrence = strstr(for_occurrence, for_keyword)) != NULL) {
         strncpy(for_occurrence, repeat_keyword, strlen(repeat_keyword));
         for_occurrence += strlen(repeat_keyword);
     }
-    
+
     return pseudocode;
 }
 
